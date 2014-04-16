1fd41d937adff003f187698a4f1acc2dd286076b
Shane StClair <shane@axiomalaska.com>
2014-04-16 13:11:01 -0700

    Support AbstractSensorML gml:id encoding/decoding

--- coding/sensorML-v101/src/test/java/org/n52/sos/decode/SensorMLDecoderV101Test.java
+++ coding/sensorML-v101/src/test/java/org/n52/sos/decode/SensorMLDecoderV101Test.java
@@ -439,4 +439,14 @@ public class SensorMLDecoderV101Test extends AbstractBeforeAfterClassTest {
         return dataArray;
     }
 
+    @Test
+    public void should_set_gml_id() throws OwsExceptionReport {
+        SensorMLDocument xbSmlDoc = getSensorMLDoc();
+        SystemType xbSystem =
+                (SystemType) xbSmlDoc.getSensorML().addNewMember().addNewProcess()
+                        .substitute(SensorMLConstants.SYSTEM_QNAME, SystemType.type);
+        xbSystem.setId(TEST_ID_1);
+        AbstractProcess absProcess = decodeAbstractProcess(xbSmlDoc);
+        assertThat(absProcess.getGmlId(), is(TEST_ID_1));
+    }    
 }

bcc60bb56c9b623a47b5a4449c32eab4099ce080
Simona Badoiu <simonaandreea.badoiu@gmail.com>
2014-08-07 00:05:13 +0300

    Add classes and interfaces for the new ArrayValue type

---
+++ hibernate/common/src/main/java/org/n52/sos/ds/hibernate/entities/interfaces/ArrayValue.java
@@ -0,0 +1,16 @@
+package org.n52.sos.ds.hibernate.entities.interfaces;
+
+import org.n52.sos.ds.hibernate.entities.HibernateRelations.HasValue;
+
+import rasj.RasGMArray;
+
+/**
+ * Interface for Hibernate boolean value entities
+ * 
+ * @author Simona Badoiu <s.a.badoiu@52north.org>
+ * @since 4.1.0
+ * 
+ */
+public interface ArrayValue extends HasValue<RasGMArray> {
+
+}

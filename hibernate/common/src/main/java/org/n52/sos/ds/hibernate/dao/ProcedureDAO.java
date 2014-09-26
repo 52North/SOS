431fbde923875972cd09d63b4eacd70dc6206a09
CarstenHollmann <c.hollmann@52north.org>
2014-09-26 14:38:04 +0200

    Add check if the resulting procedure from validProcedureTime is not null, if null return previously requested procedure

--- hibernate/common/src/main/java/org/n52/sos/ds/hibernate/dao/ProcedureDAO.java
+++ hibernate/common/src/main/java/org/n52/sos/ds/hibernate/dao/ProcedureDAO.java
@@ -173,7 +173,10 @@ public class ProcedureDAO extends AbstractIdentifierNameDescriptionDAO implement
         if (procedure instanceof TProcedure && HibernateHelper.isEntitySupported(TProcedure.class)) {
             criteria.createCriteria(TProcedure.VALID_PROCEDURE_TIME).add(Restrictions.isNull(ValidProcedureTime.END_TIME));
             LOGGER.debug("QUERY getProcedureForIdentifier(identifier): {}", HibernateHelper.getSqlString(criteria));
-            return (Procedure)criteria.uniqueResult();
+            Procedure proc = (Procedure)criteria.uniqueResult();
+            if (proc != null) {
+                return proc;
+            }
         }
         return procedure;
 

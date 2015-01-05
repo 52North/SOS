d796ea6654fdeccac2778f4e580651f57d1990f3
CarstenHollmann <c.hollmann@52north.org>
2015-01-05 14:12:18 +0100

    rename SQL script and add create and update table statements for station and network

--- misc/db/PostgreSQL/ereporting_M3_to_M4.sql
+++ misc/db/PostgreSQL/ereporting_M3_to_M4.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (C) 2012-2014 52°North Initiative for Geospatial Open Source
+-- Copyright (C) 2012-2015 52°North Initiative for Geospatial Open Source
 -- Software GmbH
 --
 -- This program is free software; you can redistribute it and/or modify it
@@ -29,3 +29,19 @@
 
 -- add published flag to series table
 ALTER TABLE public.series ADD published char(1) NOT NULL default 'T' check (published in ('T','F'));
+
+-- add station table
+create sequence public.stationId_seq;
+create table public.station (stationId int8 not null, identifier varchar(255) not null, codespace int8, name varchar(255), codespacename int8, description varchar(255), primary key (stationId));
+alter table public.station add constraint stationIdentifierUK unique (identifier);
+
+-- add network table
+create sequence public.networkId_seq;
+create table public.network (networkId int8 not null, identifier varchar(255) not null, codespace int8, name varchar(255), codespacename int8, description varchar(255), primary key (networkId));
+alter table public.network add constraint networkIdentifierUK unique (identifier);
+
+-- add station and network columns to
+alter table public.samplingPoint add column station int8;
+alter table public.samplingPoint add constraint sampPointStationFk foreign key (station) references public.station;
+alter table public.samplingPoint add column network int8;
+alter table public.samplingPoint add constraint sampPointNetworkFk foreign key (network) references public.network;

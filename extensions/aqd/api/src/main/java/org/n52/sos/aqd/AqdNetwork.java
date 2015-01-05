8ae1676015c5fa6ec84bd7ad8f0019f97ea54948
CarstenHollmann <c.hollmann@52north.org>
2015-01-05 14:13:26 +0100

    - Add SOS internal representation of AQD_Network and AQD_Station - Add network and station parameter to AqdSamplingPoint

---
+++ extensions/aqd/api/src/main/java/org/n52/sos/aqd/AqdNetwork.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2012-2015 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public
+ * License version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
+package org.n52.sos.aqd;
+
+import org.n52.sos.ef.EfEnvironmentalMonitoringNetwork;
+
+/**
+ * Class represents an AQD_Network
+ * 
+ * @author Carsten Hollmann <c.hollmann@52north.org>
+ * @since 4.3.0
+ *
+ */
+public class AqdNetwork extends EfEnvironmentalMonitoringNetwork {
+
+    private static final long serialVersionUID = -1389787928974429048L;
+
+}

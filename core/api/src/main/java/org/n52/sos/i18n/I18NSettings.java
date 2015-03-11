299804ebe0cb502973552ffeed30bafce0eb0c12
Christian Autermann <christian@autermann.org>
2015-03-11 16:52:41 +0100

    removed dummy setting

--- core/api/src/main/java/org/n52/sos/i18n/I18NSettings.java
+++ core/api/src/main/java/org/n52/sos/i18n/I18NSettings.java
@@ -35,7 +35,6 @@ import org.n52.sos.config.SettingDefinition;
 import org.n52.sos.config.SettingDefinitionGroup;
 import org.n52.sos.config.SettingDefinitionProvider;
 import org.n52.sos.config.settings.BooleanSettingDefinition;
-import org.n52.sos.config.settings.ChoiceSettingDefinition;
 import org.n52.sos.config.settings.StringSettingDefinition;
 
 import com.google.common.collect.ImmutableSet;
@@ -74,23 +73,11 @@ public class I18NSettings implements SettingDefinitionProvider {
             .setTitle("I18N show all language values")
             .setDescription(
                     "Show all language specific values if no language is queried or the queried language is not supported!");
-
-    public static final ChoiceSettingDefinition CHOICE
-            = new ChoiceSettingDefinition()
-            .addOption("a")
-            .addOption("b")
-            .addOption("c")
-            .addOption("d", "Das hier ist d")
-            .setDefaultValue("b")
-            .setKey("CHOICE_SETTING")
-            .setGroup(GROUP)
-            .setOrder(ORDER_3)
-            .setTitle("Choice Test settings")
-            .setDescription("This is a choice test setting.");
-
+    
+    
     private static final Set<SettingDefinition<?, ?>> DEFINITIONS = ImmutableSet
             .<SettingDefinition<?, ?>>of(
-                    I18N_DEFAULT_LANGUAGE_DEFINITION, SHOW_ALL_LANGUAGE_VLAUES, CHOICE);
+                    I18N_DEFAULT_LANGUAGE_DEFINITION, SHOW_ALL_LANGUAGE_VLAUES);
 
     @Override
     public Set<SettingDefinition<?, ?>> getSettingDefinitions() {

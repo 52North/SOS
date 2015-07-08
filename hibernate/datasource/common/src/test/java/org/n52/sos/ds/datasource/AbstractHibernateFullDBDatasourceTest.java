0314fdc98b24ddabfd21d48052eccadcf7f5c699
EHJ-52n <e.h.juerrens@52north.org>
2015-07-08 12:54:41 +0200

    Fix test failures

--- hibernate/datasource/common/src/test/java/org/n52/sos/ds/datasource/AbstractHibernateFullDBDatasourceTest.java
+++ hibernate/datasource/common/src/test/java/org/n52/sos/ds/datasource/AbstractHibernateFullDBDatasourceTest.java
@@ -38,13 +38,13 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
-import junit.framework.TestCase;
-
 import org.hibernate.dialect.Dialect;
 import org.hibernate.tool.hbm2ddl.DatabaseMetadata;
 import org.n52.sos.config.SettingDefinition;
 import org.n52.sos.ds.HibernateDatasourceConstants;
 import org.n52.sos.ds.hibernate.util.HibernateConstants;
+
+import junit.framework.TestCase;
 
 /**
  * @since 4.0.0
@@ -79,34 +79,31 @@ public class AbstractHibernateFullDBDatasourceTest extends TestCase {
         current.put(HibernateConstants.C3P0_MAX_SIZE, "30");
         current.put(HibernateConstants.JDBC_BATCH_SIZE, "20");
         current.put(HibernateDatasourceConstants.PROVIDED_JDBC, "true");
+        current.put(HibernateDatasourceConstants.HIBERNATE_DIRECTORY, "some-directory-stuff-to-test");
 
         final Map<String, Object> settings = ds.parseDatasourceProperties(current);
-        checkSettingKeysTransactional(settings.keySet());
+        checkSettingKeys(settings.keySet(), false, false);
     }
 
     private void checkSettingDefinitionsTransactional(final Set<SettingDefinition<?, ?>> settings) {
-        checkSettingDefinitions(settings, false);
+        checkSettingDefinitions(settings, false, true);
     }
 
     private void checkSettingDefinitionsChangableSetting(final Set<SettingDefinition<?, ?>> settings) {
-        checkSettingDefinitions(settings, true);
+        checkSettingDefinitions(settings, true, false);
 
     }
 
-    private void checkSettingDefinitions(final Set<SettingDefinition<?, ?>> settings, final boolean changeable) {
+    private void checkSettingDefinitions(final Set<SettingDefinition<?, ?>> settings, final boolean changeable, final boolean settingsDefinitions) {
         final List<String> keys = new ArrayList<String>();
         final Iterator<SettingDefinition<?, ?>> iterator = settings.iterator();
         while (iterator.hasNext()) {
             keys.add(iterator.next().getKey());
         }
-        checkSettingKeys(keys, changeable);
+        checkSettingKeys(keys, changeable, settingsDefinitions);
     }
 
-    private void checkSettingKeysTransactional(final Collection<String> keys) {
-        checkSettingKeys(keys, false);
-    }
-
-    private void checkSettingKeys(final Collection<String> keys, final boolean changeable) {
+    private void checkSettingKeys(final Collection<String> keys, final boolean changeable, final boolean settingsDefinitions) {
         boolean transactional = keys.contains(AbstractHibernateDatasource.TRANSACTIONAL_KEY);
         boolean concept = keys.contains(AbstractHibernateDatasource.DATABASE_CONCEPT_KEY);
         boolean multiLanguage = keys.contains(AbstractHibernateDatasource.MULTILINGUALISM_KEY);
@@ -120,6 +117,7 @@ public class AbstractHibernateFullDBDatasourceTest extends TestCase {
         assertTrue(keys.contains(AbstractHibernateDatasource.MIN_POOL_SIZE_KEY));
         assertTrue(keys.contains(AbstractHibernateDatasource.MAX_POOL_SIZE_KEY));
         assertTrue(keys.contains(AbstractHibernateDatasource.BATCH_SIZE_KEY));
+        assertTrue(changeable || settingsDefinitions || keys.contains(HibernateDatasourceConstants.HIBERNATE_DIRECTORY));
         assertTrue(changeable || keys.contains(AbstractHibernateDatasource.PROVIDED_JDBC_DRIVER_KEY));
         assertTrue(!transactional || keys.contains(AbstractHibernateDatasource.TRANSACTIONAL_KEY));
         assertTrue(!concept || keys.contains(AbstractHibernateDatasource.DATABASE_CONCEPT_KEY));
@@ -128,11 +126,12 @@ public class AbstractHibernateFullDBDatasourceTest extends TestCase {
         if (changeable) {
             assertEquals(9, keys.size());
         } else {
-            final int maxCount = 13;
+            final int maxCount = 14;
             int counter = maxCount;
             if (!transactional) { counter--; }
             if (!concept) { counter--; }
             if (!multiLanguage){ counter--; }
+            if (settingsDefinitions){ counter--; }
             assertEquals(counter, keys.size());
         }
     }

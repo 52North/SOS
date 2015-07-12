dc94c84b6db9c5de791d2dd2cd95c1f301c36bc3
CarstenHollmann <c.hollmann@52north.org>
2015-07-12 09:44:21 +0200

    Add transactional procedure description format in CodingRepository and use it in transactional metadata/capabilities information.

--- core/api/src/main/java/org/n52/sos/ds/AbstractUpdateSensorDescriptionDAO.java
+++ core/api/src/main/java/org/n52/sos/ds/AbstractUpdateSensorDescriptionDAO.java
@@ -51,7 +51,7 @@ public abstract class AbstractUpdateSensorDescriptionDAO extends AbstractOperati
         addProcedureParameter(opsMeta);
         if (version.equals(Sos2Constants.SERVICEVERSION)) {
             opsMeta.addPossibleValuesParameter(Sos2Constants.UpdateSensorDescriptionParams.procedureDescriptionFormat,
-                    CodingRepository.getInstance().getSupportedProcedureDescriptionFormats(service, version));
+                    CodingRepository.getInstance().getSupportedTransactionalProcedureDescriptionFormats(service, version));
         }
         opsMeta.addAnyParameterValue(Sos2Constants.UpdateSensorDescriptionParams.description);
     }

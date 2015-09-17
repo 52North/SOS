0e07dce1ef52b192cfd05d0a7c4fcbb16abf9360
CarstenHollmann <c.hollmann@52north.org>
2015-09-17 16:37:42 +0200

    Fix issue in getting substring if identifier is not an URI

--- coding/netcdf/coding/src/main/java/org/n52/sos/encode/AbstractNetcdfEncoder.java
+++ coding/netcdf/coding/src/main/java/org/n52/sos/encode/AbstractNetcdfEncoder.java
@@ -1333,7 +1333,10 @@ public abstract class AbstractNetcdfEncoder implements ObservationEncoder<Binary
         } else if (identifier.startsWith(Constants.HTTP)) {
             splitter = Constants.SLASH_STRING;
         }
-        return identifier.substring(identifier.lastIndexOf(splitter) + 1);
+        if (identifier.lastIndexOf(splitter) < identifier.length()) {
+            return identifier.substring(identifier.lastIndexOf(splitter) + 1);
+        } 
+        return identifier;
     }
 
     protected String makeDateSafe(DateTime dt) {

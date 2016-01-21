b9c5034c15f87ac23a10bf451d186b70ed483c21
CarstenHollmann <c.hollmann@52north.org>
2016-01-21 11:01:36 +0100

    Add request generation for GetObservation and GetDataAvailability in EnvironmentalMonitoringFacility

--- hibernate/feature/src/main/java/org/n52/sos/ds/hibernate/util/feature/create/EnvironmentalMonitoringFacilityStrategy.java
+++ hibernate/feature/src/main/java/org/n52/sos/ds/hibernate/util/feature/create/EnvironmentalMonitoringFacilityStrategy.java
@@ -29,6 +29,8 @@
 package org.n52.sos.ds.hibernate.util.feature.create;
 
 import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.hibernate.Session;
@@ -40,12 +42,20 @@ import org.n52.sos.ogc.gml.AbstractFeature;
 import org.n52.sos.ogc.gml.CodeWithAuthority;
 import org.n52.sos.ogc.gml.ReferenceType;
 import org.n52.sos.ogc.om.OmObservation;
+import org.n52.sos.ogc.ows.OWSConstants;
 import org.n52.sos.ogc.ows.OwsExceptionReport;
+import org.n52.sos.ogc.ows.OWSConstants.RequestParams;
+import org.n52.sos.ogc.sos.Sos2Constants;
+import org.n52.sos.ogc.sos.SosConstants;
+import org.n52.sos.service.ServiceConfiguration;
+import org.n52.sos.util.CollectionHelper;
 import org.n52.sos.util.SosHelper;
 import org.n52.sos.w3c.xlink.SimpleAttrs;
 import org.n52.svalbard.inspire.base.Identifier;
 import org.n52.svalbard.inspire.ef.ObservingCapability;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
 public class EnvironmentalMonitoringFacilityStrategy extends AbstractFeatureCreationStrategy {
@@ -56,7 +66,8 @@ public class EnvironmentalMonitoringFacilityStrategy extends AbstractFeatureCrea
 
     @Override
     public boolean apply(FeatureOfInterest feature) {
-        return EnvironmentalMonitoringFacility.class.equals(HibernateProxyHelper.getClassWithoutInitializingProxy(feature));
+        return EnvironmentalMonitoringFacility.class
+                .equals(HibernateProxyHelper.getClassWithoutInitializingProxy(feature));
     }
 
     @Override
@@ -78,72 +89,28 @@ public class EnvironmentalMonitoringFacilityStrategy extends AbstractFeatureCrea
                             getMediaMonitored(emf.getMediaMonitored()));
             addNameAndDescription(i18n, emf, emFeature, featureDAO);
             emFeature.setGeometry(createGeometry(emf, s));
-            // add measurementRegime,  mobile, operationalActivityPeriod(Set)
+            // add measurementRegime, mobile, operationalActivityPeriod(Set)
             emFeature.setMeasurementRegime(new ReferenceType(emf.getMeasurementRegime()));
             emFeature.setMobile(emf.isMobile());
-            
+
             // in table or from Data
-//            emFeature.setOperationalActivityPeriod(operationalActivityPeriod);
-            
+            // emFeature.setOperationalActivityPeriod(operationalActivityPeriod);
+
             addObservingCapabilities(emFeature, f);
             addHasObservations(emFeature, f);
-            
-//            final Set<FeatureOfInterest> parentFeatures = emf.getParents();
-//            if (parentFeatures != null && !parentFeatures.isEmpty()) {
-//                final List<AbstractFeature> sampledFeatures = new ArrayList<AbstractFeature>(parentFeatures.size());
-//                for (final FeatureOfInterest parentFeature : parentFeatures) {
-//                    sampledFeatures.add(create(parentFeature, i18n, version, s));
-//                }
-//                emFeature.setSampledFeatures(sampledFeatures);
-//            }
+
+            // final Set<FeatureOfInterest> parentFeatures = emf.getParents();
+            // if (parentFeatures != null && !parentFeatures.isEmpty()) {
+            // final List<AbstractFeature> sampledFeatures = new
+            // ArrayList<AbstractFeature>(parentFeatures.size());
+            // for (final FeatureOfInterest parentFeature : parentFeatures) {
+            // sampledFeatures.add(create(parentFeature, i18n, version, s));
+            // }
+            // emFeature.setSampledFeatures(sampledFeatures);
+            // }
             return emFeature;
         }
         return null;
-    }
-
-    private void addObservingCapabilities(org.n52.svalbard.inspire.ef.EnvironmentalMonitoringFacility emFeature,
-            FeatureOfInterest feature) {
-//      emFeature.addObservingCapability(createObservingCapability(f.getIdentifier()));
-        if (feature.hasChilds()) {
-            for (FeatureOfInterest child : feature.getChilds()) {
-//              emFeature.addObservingCapability(createObservingCapability(child.getIdentifier()));
-            }
-        }
-        
-    }
-
-    private ObservingCapability createObservingCapability(String identifier) {
-        String gdaUrl = "...&featureOfInterest=" + identifier;
-        return new ObservingCapability(gdaUrl);
-    }
-
-    private void addHasObservations(org.n52.svalbard.inspire.ef.EnvironmentalMonitoringFacility emFeature,
-            FeatureOfInterest feature) {
-        createAndAddHasObservations(emFeature, feature);
-        // check for child features
-        if (feature.hasChilds()) {
-            for (FeatureOfInterest child : feature.getChilds()) {
-                createAndAddHasObservations(emFeature, child);
-            }
-        }
-    }
-
-    private void createAndAddHasObservations(org.n52.svalbard.inspire.ef.EnvironmentalMonitoringFacility emFeature,
-            FeatureOfInterest feature) {
-        Set<String> offerings = getCache().getOfferingsForFeatureOfInterest(feature.getIdentifier());
-        if (offerings != null) {
-            for (String offering : offerings) {
-//                emFeature.addHasObservation(createObservation(feature, offering));
-            }
-        }
-    }
-
-    private OmObservation createObservation(FeatureOfInterest feature, String offering) {
-        String getObservationURL = "...&offering=" + offering;
-        SimpleAttrs simpleAttrs = new SimpleAttrs().setHref(getObservationURL);
-        OmObservation omObservation = new OmObservation();
-        omObservation.setSimpleAttrs(simpleAttrs);
-        return omObservation;
     }
 
     private Set<ReferenceType> getMediaMonitored(Set<String> mediaMonitored) {
@@ -154,6 +121,103 @@ public class EnvironmentalMonitoringFacilityStrategy extends AbstractFeatureCrea
         return referenceTypes;
     }
 
+    private void addObservingCapabilities(org.n52.svalbard.inspire.ef.EnvironmentalMonitoringFacility emFeature,
+            FeatureOfInterest feature) {
+        emFeature.addObservingCapability(createObservingCapability(feature.getIdentifier()));
+        if (feature.hasChilds()) {
+            for (FeatureOfInterest child : feature.getChilds()) {
+                emFeature.addObservingCapability(createObservingCapability(child.getIdentifier()));
+            }
+        }
 
+    }
 
+    private ObservingCapability createObservingCapability(String identifier) {
+        return new ObservingCapability(addParameter(getGetDataAvailabilityUrl(), "featureOfInterest", identifier));
+    }
+
+    private void addHasObservations(org.n52.svalbard.inspire.ef.EnvironmentalMonitoringFacility emFeature,
+            FeatureOfInterest feature) {
+        Map<String, Set<String>> featureOfferings = Maps.newHashMap();
+        featureOfferings.put(feature.getIdentifier(),
+                getCache().getOfferingsForFeatureOfInterest(feature.getIdentifier()));
+        // check for child features
+        if (feature.hasChilds()) {
+            for (FeatureOfInterest child : feature.getChilds()) {
+                Set<String> childOfferings = getCache().getOfferingsForFeatureOfInterest(child.getIdentifier());
+                if (CollectionHelper.isNotEmpty(childOfferings)) {
+                    featureOfferings.put(child.getIdentifier(), childOfferings);
+                }
+            }
+        }
+        createHasObservations(emFeature, featureOfferings);
+    }
+
+    private void createHasObservations(org.n52.svalbard.inspire.ef.EnvironmentalMonitoringFacility emFeature,
+            Map<String, Set<String>> featureOfferings) {
+        if (featureOfferings != null) {
+            for (Entry<String, Set<String>> entry : featureOfferings.entrySet()) {
+                emFeature.addHasObservation(createObservation(entry.getKey(), entry.getValue()));
+            }
+        }
+    }
+
+    private OmObservation createObservation(String feature, Set<String> offerings) {
+        String getObservationUrl = getGetObservationUrl();
+        addParameter(getObservationUrl, SosConstants.GetObservationParams.featureOfInterest.name(), feature);
+        addParameter(getObservationUrl, SosConstants.GetObservationParams.offering.name(), offerings);
+        SimpleAttrs simpleAttrs = new SimpleAttrs().setHref(getObservationUrl);
+        OmObservation omObservation = new OmObservation();
+        omObservation.setSimpleAttrs(simpleAttrs);
+        return omObservation;
+    }
+
+    private OmObservation createObservation(String featureOfInterest, String offering) {
+        SimpleAttrs simpleAttrs = new SimpleAttrs().setHref(
+                addParameter(getGetObservationUrl(), SosConstants.GetObservationParams.offering.name(), offering));
+        OmObservation omObservation = new OmObservation();
+        omObservation.setSimpleAttrs(simpleAttrs);
+        return omObservation;
+    }
+
+    private String getGetDataAvailabilityUrl() {
+        return new StringBuilder(getBaseGetUrl()).append(getRequest("GetDataAvailability")).toString();
+    }
+
+    private String getGetObservationUrl() {
+        return new StringBuilder(getBaseGetUrl()).append(getRequest(SosConstants.Operations.GetObservation.name()))
+                .toString();
+    }
+
+    private String getBaseGetUrl() {
+        final StringBuilder url = new StringBuilder();
+        // service URL
+        url.append(getServiceConfiguration().getServiceURL());
+        // ?
+        url.append('?');
+        // service
+        url.append(OWSConstants.RequestParams.service.name()).append('=').append(SosConstants.SOS);
+        // version
+        url.append('&').append(OWSConstants.RequestParams.version.name()).append('=')
+                .append(Sos2Constants.SERVICEVERSION);
+        return url.toString();
+    }
+
+    private String getRequest(String requestName) {
+        return new StringBuilder().append('&').append(RequestParams.request.name()).append('=').append(requestName)
+                .toString();
+    }
+
+    private String addParameter(String url, String parameter, String value) {
+        return new StringBuilder(url).append('&').append(parameter).append('=').append(value).toString();
+    }
+
+    private String addParameter(String url, String parameter, Set<String> offerings) {
+        return new StringBuilder(url).append('&').append(parameter).append('=').append(Joiner.on(',').join(offerings))
+                .toString();
+    }
+
+    private ServiceConfiguration getServiceConfiguration() {
+        return ServiceConfiguration.getInstance();
+    }
 }

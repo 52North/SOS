8383a6fdc320e8af2aefb903d91de3484d38f8d1
CarstenHollmann <c.hollmann@52north.org>
2016-09-27 10:53:35 +0200

    Fix installation issue if time string format is not set

--- hibernate/datasource/common/src/main/java/org/n52/sos/ds/datasource/AbstractHibernateFullDBDatasource.java
+++ hibernate/datasource/common/src/main/java/org/n52/sos/ds/datasource/AbstractHibernateFullDBDatasource.java
@@ -136,9 +136,15 @@ public abstract class AbstractHibernateFullDBDatasource extends AbstractHibernat
         p.put(HibernateConstants.CONNECTION_TEST_ON_BORROW, "true");
         p.put(PROVIDED_JDBC, settings.get(PROVIDED_JDBC_DRIVER_KEY).toString());
         p.put(DATABASE_CONCEPT_KEY, settings.get(DATABASE_CONCEPT_KEY));
-        p.put(HIBERNATE_DATASOURCE_TIMEZONE, settings.get(TIMEZONE_KEY));
-        p.put(HIBERNATE_DATASOURCE_TIME_STRING_FORMAT, settings.get(TIME_STRING_FORMAT_KEY));
-        p.put(HIBERNATE_DATASOURCE_TIME_STRING_Z, settings.get(TIME_STRING_Z_KEY).toString());
+        if (settings.containsKey(TIMEZONE_KEY)) {
+            p.put(HIBERNATE_DATASOURCE_TIMEZONE, settings.get(TIMEZONE_KEY));
+        }
+        if (settings.containsKey(TIME_STRING_FORMAT_KEY)) {
+            p.put(HIBERNATE_DATASOURCE_TIME_STRING_FORMAT, settings.get(TIME_STRING_FORMAT_KEY));
+        }
+        if (settings.containsKey(TIME_STRING_Z_KEY)) {
+            p.put(HIBERNATE_DATASOURCE_TIME_STRING_Z, settings.get(TIME_STRING_Z_KEY).toString());
+        }
         addMappingFileDirectories(settings, p);
 
         return p;
@@ -161,10 +167,15 @@ public abstract class AbstractHibernateFullDBDatasource extends AbstractHibernat
         settings.put(DATABASE_CONCEPT_KEY,  current.getProperty(DATABASE_CONCEPT_KEY));
         settings.put(PROVIDED_JDBC_DRIVER_KEY,
                 current.getProperty(PROVIDED_JDBC, PROVIDED_JDBC_DRIVER_DEFAULT_VALUE.toString()));
-        
-        settings.put(TIMEZONE_KEY, current.getProperty(HIBERNATE_DATASOURCE_TIMEZONE));
-        settings.put(TIME_STRING_FORMAT_KEY, current.getProperty(HIBERNATE_DATASOURCE_TIME_STRING_FORMAT));
-        settings.put(TIME_STRING_Z_KEY, Boolean.valueOf(current.getProperty(HIBERNATE_DATASOURCE_TIME_STRING_Z)));
+        if (current.containsKey(HIBERNATE_DATASOURCE_TIMEZONE)) {
+            settings.put(TIMEZONE_KEY, current.getProperty(HIBERNATE_DATASOURCE_TIMEZONE));
+        }
+        if (current.containsKey(HIBERNATE_DATASOURCE_TIME_STRING_FORMAT)) {
+            settings.put(TIME_STRING_FORMAT_KEY, current.getProperty(HIBERNATE_DATASOURCE_TIME_STRING_FORMAT));
+        }
+        if (current.containsKey(HIBERNATE_DATASOURCE_TIME_STRING_Z)) {
+            settings.put(TIME_STRING_Z_KEY, Boolean.valueOf(current.getProperty(HIBERNATE_DATASOURCE_TIME_STRING_Z)));
+        }
         final String url = current.getProperty(HibernateConstants.CONNECTION_URL);
 
         final String[] parsed = parseURL(url);
--- hibernate/datasource/common/src/test/java/org/n52/sos/ds/datasource/AbstractHibernateFullDBDatasourceTest.java
+++ hibernate/datasource/common/src/test/java/org/n52/sos/ds/datasource/AbstractHibernateFullDBDatasourceTest.java
@@ -87,28 +87,28 @@ public class AbstractHibernateFullDBDatasourceTest extends TestCase {
         current.put(HibernateDatasourceConstants.HIBERNATE_DIRECTORY, "some-directory-stuff-to-test");
 
         final Map<String, Object> settings = ds.parseDatasourceProperties(current);
-        checkSettingKeys(settings.keySet(), false, false);
+        checkSettingKeys(settings.keySet(), false, false, false);
     }
 
     private void checkSettingDefinitionsTransactional(final Set<SettingDefinition<?, ?>> settings) {
-        checkSettingDefinitions(settings, false, true);
+        checkSettingDefinitions(settings, false, true, true);
     }
 
     private void checkSettingDefinitionsChangableSetting(final Set<SettingDefinition<?, ?>> settings) {
-        checkSettingDefinitions(settings, true, false);
+        checkSettingDefinitions(settings, true, false, false);
 
     }
 
-    private void checkSettingDefinitions(final Set<SettingDefinition<?, ?>> settings, final boolean changeable, final boolean settingsDefinitions) {
+    private void checkSettingDefinitions(final Set<SettingDefinition<?, ?>> settings, final boolean changeable, final boolean settingsDefinitions, final boolean timeFormat) {
         final List<String> keys = new ArrayList<>();
         final Iterator<SettingDefinition<?, ?>> iterator = settings.iterator();
         while (iterator.hasNext()) {
             keys.add(iterator.next().getKey());
         }
-        checkSettingKeys(keys, changeable, settingsDefinitions);
+        checkSettingKeys(keys, changeable, settingsDefinitions, timeFormat);
     }
 
-    private void checkSettingKeys(final Collection<String> keys, final boolean changeable, final boolean settingsDefinitions) {
+    private void checkSettingKeys(final Collection<String> keys, final boolean changeable, final boolean settingsDefinitions, final boolean timeFormat) {
         boolean transactional = keys.contains(AbstractHibernateDatasource.TRANSACTIONAL_KEY);
         boolean concept = keys.contains(AbstractHibernateDatasource.DATABASE_CONCEPT_KEY);
         boolean multiLanguage = keys.contains(AbstractHibernateDatasource.MULTILINGUALISM_KEY);
@@ -127,6 +127,9 @@ public class AbstractHibernateFullDBDatasourceTest extends TestCase {
         assertTrue(!transactional || keys.contains(AbstractHibernateDatasource.TRANSACTIONAL_KEY));
         assertTrue(!concept || keys.contains(AbstractHibernateDatasource.DATABASE_CONCEPT_KEY));
         assertTrue(!multiLanguage || keys.contains(AbstractHibernateDatasource.MULTILINGUALISM_KEY));
+        assertTrue(!timeFormat || keys.contains(AbstractHibernateDatasource.TIMEZONE_KEY));
+        assertTrue(!timeFormat || keys.contains(AbstractHibernateDatasource.TIME_STRING_FORMAT_KEY));
+        assertTrue(!timeFormat || keys.contains(AbstractHibernateDatasource.TIME_STRING_Z_KEY));
 
         if (changeable) {
             assertEquals(CHANGEABLE_COUNT, keys.size());
@@ -136,6 +139,7 @@ public class AbstractHibernateFullDBDatasourceTest extends TestCase {
             if (!concept) { counter--; }
             if (!multiLanguage){ counter--; }
             if (settingsDefinitions){ counter--; }
+            if (!timeFormat){ counter-= 3; }
             assertEquals(counter, keys.size());
         }
     }

c20db1c01900329d9fb1891486f7aa007abcae5b
CarstenHollmann <c.hollmann@52north.org>
2016-11-08 11:48:02 +0100

    Rename package

--- extensions/inspire/code/src/main/java/org/n52/sos/convert/InspireObservationResponseConverter.java
+++ extensions/inspire/code/src/main/java/org/n52/sos/convert/InspireObservationResponseConverter.java
@@ -26,7 +26,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
  */
-package org.n52.sos.converter;
+package org.n52.sos.convert;
 
 import java.util.Collection;
 import java.util.Collections;

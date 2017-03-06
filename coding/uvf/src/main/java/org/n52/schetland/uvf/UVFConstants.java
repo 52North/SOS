e1f43109566e21c0753aa8095c6d4c493b0adf4b
EHJ-52n <e.h.juerrens@52north.org>
2017-03-06 12:17:48 +0100

    Add documentation of TIME_FORMAT constant

--- coding/uvf/src/main/java/org/n52/schetland/uvf/UVFConstants.java
+++ coding/uvf/src/main/java/org/n52/schetland/uvf/UVFConstants.java
@@ -5,6 +5,11 @@ import org.n52.sos.util.http.MediaType;
 public interface UVFConstants {
 
     MediaType CONTENT_TYPE_UVF = new MediaType("application", "uvf");
+
+    /**
+     * Time format to be used in UVF encoded data: <code>yyMMddhhmm</code>,
+     * e.g. <code>7001011230</code> is 01.01.1970 12:30 UTC
+     */
     String TIME_FORMAT = "yyMMddhhmm";
     
     /**

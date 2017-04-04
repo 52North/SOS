8ade472916b292e36c91568c037cc8039ed6348a
CarstenHollmann <c.hollmann@52north.org>
2017-04-04 14:25:23 +0200

    Fix time format which now encodes hours as 24 value

--- coding/uvf/src/main/java/org/n52/schetland/uvf/UVFConstants.java
+++ coding/uvf/src/main/java/org/n52/schetland/uvf/UVFConstants.java
@@ -11,10 +11,10 @@ public interface UVFConstants {
     MediaType CONTENT_TYPE_UVF = new MediaType("application", "uvf");
 
     /**
-     * Time format to be used in UVF encoded data: <code>yyMMddhhmm</code>,
+     * Time format to be used in UVF encoded data: <code>yyMMddHHmm</code>,
      * e.g. <code>7001011230</code> is 01.01.1970 12:30 UTC
      */
-    String TIME_FORMAT = "yyMMddhhmm";
+    String TIME_FORMAT = "yyMMddHHmm";
     
     /**
      * The identifiers length is limited to 15 characters following UVF spec for lines 2, 3

99dcf87d743602bc010f688fc6bbd42b2bce2859
Henning Bredel <bredel@52north.org>
2017-04-10 12:42:24 +0200

    add view creations to merge fois

---
+++ misc/db/PostgreSQL/uba/create_foi_with_roles.sql
@@ -0,0 +1,22 @@
+﻿CREATE MATERIALIZED VIEW public.foi_with_mergerole AS 
+ SELECT DISTINCT foi1.featureofinterestid,
+        CASE
+            WHEN NOT (foi1.featureofinterestid IN ( SELECT foi0.featureofinterestid
+               FROM mergable_foi foi0)) OR (foi1.featureofinterestid IN ( SELECT max(foi0.featureofinterestid) AS max
+               FROM mergable_foi foi0
+                 JOIN featureofinterest foi1_1 ON foi0.geom = foi1_1.geom
+              GROUP BY foi0.geom)) THEN 'master'::text
+            ELSE 'slave'::text
+        END AS merge_role,
+    foi1.hibernatediscriminator,
+    foi1.featureofinteresttypeid,
+    foi1.identifier,
+    foi1.codespace,
+    foi1.name,
+    foi1.codespacename,
+    foi1.description,
+    foi1.geom,
+    foi1.descriptionxml,
+    foi1.url
+   FROM featureofinterest foi1
+WITH DATA;
---
+++ misc/db/PostgreSQL/uba/create_mergable_foi_view.sql
@@ -0,0 +1,20 @@
+﻿CREATE MATERIALIZED VIEW public.mergable_foi AS 
+ SELECT DISTINCT foi.featureofinterestid,
+    foi.hibernatediscriminator,
+    foi.featureofinteresttypeid,
+    foi.identifier,
+    foi.codespace,
+    foi.name,
+    foi.codespacename,
+    foi.description,
+    foi.geom,
+    foi.descriptionxml,
+    foi.url
+   FROM featureofinterest foi
+  WHERE (foi.geom IN ( SELECT g0.geom
+           FROM ( SELECT featureofinterest.geom,
+                    count(*) AS num
+                   FROM featureofinterest
+                  GROUP BY featureofinterest.geom) g0
+          WHERE g0.num <> 1))
+WITH DATA;

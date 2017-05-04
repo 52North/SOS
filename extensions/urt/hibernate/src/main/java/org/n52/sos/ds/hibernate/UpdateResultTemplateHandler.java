6801808cfdbb641e14b53173270b3dd1f91ae345
EHJ-52n <e.h.juerrens@52north.org>
2017-05-04 12:53:16 +0200

    first working version: encoding is updated and used as expected

--- extensions/urt/hibernate/src/main/java/org/n52/sos/ds/hibernate/UpdateResultTemplateHandler.java
+++ extensions/urt/hibernate/src/main/java/org/n52/sos/ds/hibernate/UpdateResultTemplateHandler.java
@@ -31,9 +31,14 @@ package org.n52.sos.ds.hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.n52.shetland.ogc.sos.urt.UpdateResultTemplateConstants;
 import org.n52.sos.ds.AbstractUpdateResultTemplateHandler;
 import org.n52.sos.ds.HibernateDatasourceConstants;
 import org.n52.sos.ds.hibernate.dao.ResultTemplateDAO;
+import org.n52.sos.ds.hibernate.entities.ResultTemplate;
+import org.n52.sos.exception.CodedException;
+import org.n52.sos.exception.ows.InvalidParameterValueException;
+import org.n52.sos.exception.ows.MissingParameterValueException;
 import org.n52.sos.exception.ows.NoApplicableCodeException;
 import org.n52.sos.ogc.ows.OwsExceptionReport;
 import org.n52.sos.ogc.sos.SosConstants;
@@ -41,34 +46,47 @@ import org.n52.sos.request.UpdateResultTemplateRequest;
 import org.n52.sos.response.UpdateResultTemplateResponse;
 import org.n52.sos.util.http.HTTPStatus;
 
-public class UpdateResultTemplateHandler extends AbstractUpdateResultTemplateHandler {
+public class UpdateResultTemplateHandler
+        extends AbstractUpdateResultTemplateHandler {
 
-    private final HibernateSessionHolder sessionHolder = new HibernateSessionHolder();
+    private String resultTemplateId = "";
+    private ResultTemplate resultTemplate = null;
+        
+    private final HibernateSessionHolder sessionHolder =
+            new HibernateSessionHolder();
 
     private final ResultTemplateDAO resultTemplateDAO = new ResultTemplateDAO();
-    private String deletedResultTemplate;
-
+    
     public UpdateResultTemplateHandler() {
         super(SosConstants.SOS);
     }
+
     @Override
     public String getDatasourceDaoIdentifier() {
         return HibernateDatasourceConstants.ORM_DATASOURCE_DAO_IDENTIFIER;
     }
 
     @Override
-    public UpdateResultTemplateResponse updateResultTemplate(UpdateResultTemplateRequest request)
+    public UpdateResultTemplateResponse updateResultTemplate(
+            UpdateResultTemplateRequest request)
             throws OwsExceptionReport {
         Session session = null;
         Transaction transaction = null;
+        boolean updated = false;
         try {
             session = sessionHolder.getSession();
             transaction = session.beginTransaction();
+            checkAndGetResultTemplate(request, session);
             if (request.isSetResultStructure()) {
-                updateResultStructure(request.getResultStructure(), session);
+                resultTemplate.setResultStructure(request.getResultStructure());
+                updated = true;
             }
             if (request.isSetResultEncoding()) {
-                updateResultEncoding(request.getResultEncoding(), session);
+                resultTemplate.setResultEncoding(request.getResultEncoding());
+                updated = true;
+            }
+            if (updated) {
+                session.save(resultTemplate);
             }
             session.flush();
             transaction.commit();
@@ -80,24 +98,36 @@ public class UpdateResultTemplateHandler extends AbstractUpdateResultTemplateHan
         } finally {
             sessionHolder.returnSession(session);
         }
-        return request.getResponse().setUpdatedResultTemplate(deletedResultTemplate);
+        return request.getResponse().setUpdatedResultTemplate(resultTemplateId);
     }
 
-    protected void handleHibernateException(HibernateException he) throws OwsExceptionReport {
-        HTTPStatus status = HTTPStatus.INTERNAL_SERVER_ERROR;
+    private void checkAndGetResultTemplate(UpdateResultTemplateRequest request, Session session) throws CodedException {
+        if (request.isSetResultTemplate()) {
+            resultTemplateId = request.getResultTemplate();
+            resultTemplate = resultTemplateDAO.getResultTemplateObject(
+                    resultTemplateId,
+                    session);
+            // check result template object
+            if (resultTemplate == null) {
+                throw new InvalidParameterValueException(
+                        UpdateResultTemplateConstants.PARAMS.resultTemplate,
+                        resultTemplateId)
+                        .withMessage("Could not retrieve result template "
+                                + "object from database with id '%s'.",
+                                resultTemplateId);
+            }
+        } else {
+            throw new MissingParameterValueException(
+                    UpdateResultTemplateConstants.PARAMS.resultTemplate);
+        }
+    }
+
+    protected void handleHibernateException(HibernateException he)
+            throws OwsExceptionReport {
         throw new NoApplicableCodeException()
                 .causedBy(he)
                 .withMessage("Error while updating result template!")
-                .setStatus(status);
+                .setStatus(HTTPStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private void updateResultStructure(String resultStructure, Session session) {
-        // TODO implement updateResultStructure()
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    private void updateResultEncoding(String resultEncoding, Session session) {
-        // TODO implement updateResultEncoding(...)
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
 }

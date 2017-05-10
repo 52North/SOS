5385c5b11b5f0a6f3997835187cdefab5d13586c
CarstenHollmann <c.hollmann@52north.org>
2017-05-10 15:52:41 +0200

    Update SQL scripts

--- misc/db/PostgreSQL/series/PG_update_43_44.sql
+++ misc/db/PostgreSQL/series/PG_update_43_44.sql
@@ -27,53 +27,97 @@
 -- Public License for more details.
 --
 
-ALTER TABLE public."procedure" ADD COLUMN istype char(1) default 'F' check (istype in ('T','F'));
-ALTER TABLE public."procedure" ADD COLUMN isaggregation char(1) default 'F' check (isaggregation in ('T','F'));
-
-ALTER TABLE public."procedure" ADD COLUMN typeof int8;
-
-alter table public."procedure" add constraint typeoffk foreign key (procedureid) references public."procedure";
-
--- complex observation
-ALTER TABLE public.observableproperty ADD COLUMN hiddenchild char(1) default 'F' check (hiddenchild in ('T','F'));
-
-ALTER TABLE public.observation ADD COLUMN child char(1) default 'F' check (child in ('T','F'));
-ALTER TABLE public.observation ADD COLUMN parent char(1) default 'F' check (parent in ('T','F'));
-
-create table public.complexValue (observationId int8 not null, primary key (observationId));
-alter table public.complexValue add constraint observationComplexValueFk foreign key (observationId) references public.observation;
-
-create table public.compositeObservation (observationId int8 not null, childObservationId int8 not null, primary key (observationId, childObservationId));
-alter table public.compositeObservation add constraint observationChildFk foreign key (childObservationId) references public.observation;
-
--- spatial index
-create index featureGeomIdx on public.featureOfInterest USING GIST (geom);
+-- Add and update other columns
+alter table public."procedure" add column typeOf int8;
+alter table public."procedure" add column isType char(1) default 'F' check(isType in ('T','F'));
+alter table public."procedure" add column isAggregation char(1) default 'T' check(isAggregation in ('T','F'));
+alter table public."procedure" add column mobile char(1) default 'F' check(mobile in ('T','F'));
+alter table public."procedure" add column insitu char(1) default 'T' check(insitu in ('T','F'));
+create table public.booleanfeatparamvalue (parameterId int8 not null, value char(1), primary key (parameterId), check (value in ('T','F')));
+create table public.booleanparametervalue (parameterId int8 not null, value char(1), primary key (parameterId), check (value in ('T','F')));
+create table public.categoryfeatparamvalue (parameterId int8 not null, value varchar(255), unitId int8, primary key (parameterId));
+create table public.categoryparametervalue (parameterId int8 not null, value varchar(255), unitId int8, primary key (parameterId));
+alter table public.categoryvalue add column identifier varchar(255);
+alter table public.categoryvalue add column name varchar(255);
+alter table public.categoryvalue add column description varchar(255);
+create table public.complexvalue (observationId int8 not null, primary key (observationId));
+create table public.compositeobservation (observationId int8 not null, childObservationId int8 not null, primary key (observationId, childObservationId));
+create table public.countfeatparamvalue (parameterId int8 not null, value int4, primary key (parameterId));
+create table public.countparametervalue (parameterId int8 not null, value int4, primary key (parameterId));
+create table public.featureparameter (parameterId int8 not null, featureOfInterestId int8 not null, name varchar(255) not null, primary key (parameterId));
+create table public.numericfeatparamvalue (parameterId int8 not null, value float8, unitId int8, primary key (parameterId));
+create table public.numericparametervalue (parameterId int8 not null, value float8, unitId int8, primary key (parameterId));
+alter table public.observableproperty add column hiddenChild char(1) default 'F' not null check(hiddenChild in ('T','F'));
+alter table public.observation add column child char(1) default 'F' not null check(child in ('T','F'));
+alter table public.observation add column parent char(1) default 'F' not null check(parent in ('T','F'));
+alter table public.observationconstellation add column disabled char(1) default 'F' not null check(disabled in ('T','F'));
+create table public.offeringrelation (parentOfferingId int8 not null, childOfferingId int8 not null, primary key (childOfferingId, parentOfferingId));
+alter table public.parameter add column name varchar(255) not null;
+create table public.profileobservation (observationId int8 not null, childObservationId int8 not null, primary key (observationId, childObservationId));
+create table public.profilevalue (observationId int8 not null, fromlevel float8, tolevel float8, levelunitid int8, primary key (observationId));
+create table public.relatedobservation (relatedObservationId int8 not null, observationId int8, relatedObservation int8, role varchar(255), relatedUrl varchar(255), primary key (relatedObservationId));
+alter table public.series add column hiddenChild char(1) default 'F' not null check(hiddenChild in ('T','F'));
+alter table public.series add column identifier varchar(255);
+alter table public.series add column codespace int8;
+alter table public.series add column name varchar(255);
+alter table public.series add column codespaceName int8;
+alter table public.series add column description varchar(255);
+alter table public.series add column seriesType varchar(255);
+create table public.textfeatparamvalue (parameterId int8 not null, value varchar(255), primary key (parameterId));
+create table public.textparametervalue (parameterId int8 not null, value varchar(255), primary key (parameterId));
+alter table public.textvalue add column identifier varchar(255);
+alter table public.textvalue add column name varchar(255);
+alter table public.textvalue add column description varchar(255);
+alter table public.unit add column name varchar(255);
+alter table public.unit add column link varchar(255);
+create table public.xmlfeatparamvalue (parameterId int8 not null, value text, primary key (parameterId));
+create table public.xmlparametervalue (parameterId int8 not null, value text, primary key (parameterId));
+create index booleanFeatParamIdx on public.booleanfeatparamvalue (value);
+create index booleanParamIdx on public.booleanparametervalue (value);
+create index categoryFeatParamIdx on public.categoryfeatparamvalue (value);
+create index categoryParamIdx on public.categoryparametervalue (value);
+create index countFeatParamIdx on public.countfeatparamvalue (value);
+create index countParamIdx on public.countparametervalue (value);
+create index featureGeomIdx on public.featureofinterest USING GIST (geom);
+create index featParamNameIdx on public.featureparameter (name);
+create index quantityFeatParamIdx on public.numericfeatparamvalue (value);
+create index quantityParamIdx on public.numericparametervalue (value);
 create index samplingGeomIdx on public.observation USING GIST (samplingGeometry);
-
--- XML based parameter values
-CREATE TABLE public.xmlparametervalue (parameterid bigint PRIMARY KEY, value text);
-ALTER TABLE public.xmlparametervalue ADD CONSTRAINT parameterxmlvaluefk FOREIGN KEY (parameterid) REFERENCES public.parameter (parameterid);
-CREATE INDEX xmlparamidx ON public.xmlparametervalue USING btree (value);
-
--- Offering relations
-CREATE TABLE offeringrelation
-(
-  parentofferingid bigint NOT NULL, -- Foreign Key (FK) to the related parent offering. Contains "offering".offeringid
-  childofferingid bigint NOT NULL, -- Foreign Key (FK) to the related child offering. Contains "offering".offeringid
-  CONSTRAINT offeringrelation_pkey PRIMARY KEY (childofferingid, parentofferingid),
-  CONSTRAINT offeringchildfk FOREIGN KEY (childofferingid)
-      REFERENCES offering (offeringid) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT offeringparenffk FOREIGN KEY (parentofferingid)
-      REFERENCES offering (offeringid) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE offeringrelation
-  OWNER TO postgres;
-COMMENT ON TABLE offeringrelation
-  IS 'Relation table to store offering hierarchies. E.g. define a parent in a query and all childs are also contained in the response. Mapping file: mapping/core/Offering.hbm.xml';
-COMMENT ON COLUMN offeringrelation.parentofferingid IS 'Foreign Key (FK) to the related parent offering. Contains "offering".offeringid';
-COMMENT ON COLUMN offeringrelation.childofferingid IS 'Foreign Key (FK) to the related child offering. Contains "offering".offeringid';
+create index paramNameIdx on public.parameter (name);
+create index relatedObsObsIdx on public.relatedobservation (observationId);
+alter table public.series add constraint seriesIdentifierUK unique (identifier);
+create index textFeatParamIdx on public.textfeatparamvalue (value);
+create index textParamIdx on public.textparametervalue (value);
+alter table public."procedure" add constraint typeOfFk foreign key (typeOf) references public."procedure";
+alter table public.booleanfeatparamvalue add constraint featParamBooleanValueFk foreign key (parameterId) references public.featureparameter;
+alter table public.booleanparametervalue add constraint parameterBooleanValueFk foreign key (parameterId) references public.parameter;
+alter table public.categoryfeatparamvalue add constraint featParamCategoryValueFk foreign key (parameterId) references public.featureparameter;
+alter table public.categoryfeatparamvalue add constraint catfeatparamvalueUnitFk foreign key (unitId) references public.unit;
+alter table public.categoryparametervalue add constraint parameterCategoryValueFk foreign key (parameterId) references public.parameter;
+alter table public.categoryparametervalue add constraint catParamValueUnitFk foreign key (unitId) references public.unit;
+alter table public.complexvalue add constraint observationComplexValueFk foreign key (observationId) references public.observation;
+alter table public.compositeobservation add constraint observationChildFk foreign key (childObservationId) references public.observation;
+alter table public.compositeobservation add constraint observationParentFK foreign key (observationId) references public.complexvalue;
+alter table public.countfeatparamvalue add constraint featParamCountValueFk foreign key (parameterId) references public.featureparameter;
+alter table public.countparametervalue add constraint parameterCountValueFk foreign key (parameterId) references public.parameter;
+alter table public.featureparameter add constraint FK_4ps6yv41rwnbu3q0let2v7foreign key (featureOfInterestId) references public.featureofinterest;
+alter table public.numericfeatparamvalue add constraint featParamNumericValueFk foreign key (parameterId) references public.featureparameter;
+alter table public.numericfeatparamvalue add constraint quanfeatparamvalueUnitFk foreign key (unitId) references public.unit;
+alter table public.numericparametervalue add constraint parameterNumericValueFk foreign key (parameterId) references public.parameter;
+alter table public.numericparametervalue add constraint quanParamValueUnitFk foreign key (unitId) references public.unit;
+alter table public.offeringrelation add constraint offeringChildFk foreign key (childOfferingId) references public.offering;
+alter table public.offeringrelation add constraint offeringParenfFk foreign key (parentOfferingId) references public.offering;
+alter table public.parameter add constraint FK_3v5iovcndi9w0hgh827hcvivw foreign key (observationId) references public.observation;
+alter table public.profileobservation add constraint profileObsChildFk foreign key (childObservationId) references public.observation;
+alter table public.profileobservation add constraint profileObsParentFK foreign key (observationId) references public.profilevalue;
+alter table public.profilevalue add constraint observationProfileValueFk foreign key (observationId) references public.observation;
+alter table public.profilevalue add constraint profileUnitFk foreign key (levelunitid) references public.unit;
+alter table public.relatedobservation add constraint FK_g0f0mpuxn3co65uwud4pwxh4q foreign key (observationId) references public.observation;
+alter table public.relatedobservation add constraint FK_m4nuof4x6w253biuu1r6ttnqc foreign key (relatedObservation) references public.observation;
+alter table public.series add constraint seriesCodespaceIdentifierFk foreign key (codespace) references public.codespace;
+alter table public.series add constraint seriesCodespaceNameFk foreign key (codespaceName) references public.codespace;
+alter table public.textfeatparamvalue add constraint featParamTextValueFk foreign key (parameterId) references public.featureparameter;
+alter table public.textparametervalue add constraint parameterTextValueFk foreign key (parameterId) references public.parameter;
+alter table public.xmlfeatparamvalue add constraint featParamXmlValueFk foreign key (parameterId) references public.featureparameter;
+alter table public.xmlparametervalue add constraint parameterXmlValueFk foreign key (parameterId) references public.parameter;
+create sequence public.relatedObservationId_seq
--- misc/db/SqlServer/series/SqlServer_update_43_44.sql
+++ misc/db/SqlServer/series/SqlServer_update_43_44.sql
@@ -27,26 +27,97 @@
 -- Public License for more details.
 --
 
-ALTER TABLE dbo.[procedure] ADD istype char(1) default 'T' check (istype in ('T','F'));
-ALTER TABLE dbo.[procedure] ADD isaggregation char(1) default 'T' check (isaggregation in ('T','F'));
-
-ALTER TABLE dbo.[procedure] ADD typeof bigint;
-
-ALTER TABLE dbo.[procedure] add constraint typeoffk foreign key (typeof) references dbo.[procedure];
-
--- complex observation
-ALTER TABLE dbo.observableproperty ADD COLUMN hiddenchild char(1) default 'F' check (hiddenchild in ('T','F'));
-
-ALTER TABLE dbo.observation ADD COLUMN child char(1) default 'F' check (child in ('T','F'));
-ALTER TABLE dbo.observation ADD COLUMN parent char(1) default 'F' check (parent in ('T','F'));
-
-create table dbo.complexValue (observationId bigint not null, primary key (observationId));
-alter table dbo.complexValue add constraint observationComplexValueFk foreign key (observationId) references dbo.observation;
-
-create table dbo.compositeObservation (observationId bigint not null, childObservationId bigint not null, primary key (observationId, childObservationId));
-alter table dbo.compositeObservation add constraint observationChildFk foreign key (childObservationId) references dbo.observation;
-alter table dbo.compositeObservation add constraint observationParentFK foreign key (observationId) references dbo.complexValue;
-
--- spatial index
-create spatial index featureGeomIdx on sdo.featureOfInterest (geom);
-create spatial index samplingGeomIdx on sdo.observation (samplingGeometry);
+-- Add and update other columns
+alter table dbo.[procedure] add column typeOf int8;
+alter table dbo.[procedure] add column isType char(1) default 'F' check(isType in ('T','F'));
+alter table dbo.[procedure] add column isAggregation char(1) default 'T' check(isAggregation in ('T','F'));
+alter table dbo.[procedure] add column mobile char(1) default 'F' check(mobile in ('T','F'));
+alter table dbo.[procedure] add column insitu char(1) default 'T' check(insitu in ('T','F'));
+create table dbo.booleanfeatparamvalue (parameterId bigint not null, value char(1), primary key (parameterId), check (value in ('T','F')));
+create table dbo.booleanparametervalue (parameterId bigint not null, value char(1), primary key (parameterId), check (value in ('T','F')));
+create table dbo.categoryfeatparamvalue (parameterId bigint not null, value varchar(255), unitId bigint, primary key (parameterId));
+create table dbo.categoryparametervalue (parameterId bigint not null, value varchar(255), unitId bigint, primary key (parameterId));
+alter table dbo.categoryvalue add column identifier varchar(255);
+alter table dbo.categoryvalue add column name varchar(255);
+alter table dbo.categoryvalue add column description varchar(255);
+create table dbo.complexvalue (observationId bigint not null, primary key (observationId));
+create table dbo.compositeobservation (observationId bigint not null, childObservationId bigint not null, primary key (observationId, childObservationId));
+create table dbo.countfeatparamvalue (parameterId bigint not null, value int, primary key (parameterId));
+create table dbo.countparametervalue (parameterId bigint not null, value int, primary key (parameterId));
+create table dbo.featureparameter (parameterId bigint not null, featureOfInterestId bigint not null, name varchar(255) not null, primary key (parameterId));
+create table dbo.numericfeatparamvalue (parameterId bigint not null, value float, unitId bigint, primary key (parameterId));
+create table dbo.numericparametervalue (parameterId bigint not null, value float, unitId bigint, primary key (parameterId));
+alter table dbo.observableproperty add column hiddenChild char(1) default 'F' not null check(hiddenChild in ('T','F'));
+alter table dbo.observation add column child char(1) default 'F' not null check(child in ('T','F'));
+alter table dbo.observation add column parent char(1) default 'F' not null check(parent in ('T','F'));
+alter table dbo.observationconstellation add column disabled char(1) default 'F' not null check(disabled in ('T','F'));
+create table dbo.offeringrelation (parentOfferingId bigint not null, childOfferingId bigint not null, primary key (childOfferingId, parentOfferingId));
+alter table dbo.parameter add column name varchar(255) not null;
+create table dbo.profileobservation (observationId bigint not null, childObservationId bigint not null, primary key (observationId, childObservationId));
+create table dbo.profilevalue (observationId bigint not null, fromlevel float, tolevel float, levelunitid bigint, primary key (observationId));
+create table dbo.relatedobservation (relatedObservationId bigint not null, observationId bigint, relatedObservation bigint, role varchar(255), relatedUrl varchar(255), primary key (relatedObservationId));
+alter table dbo.series add column hiddenChild char(1) default 'F' not null check(hiddenChild in ('T','F'));
+alter table dbo.series add column identifier varchar(255);
+alter table dbo.series add column codespace int8;
+alter table dbo.series add column name varchar(255);
+alter table dbo.series add column codespaceName int8;
+alter table dbo.series add column description varchar(255);
+alter table dbo.series add column seriesType varchar(255);
+create table dbo.textfeatparamvalue (parameterId bigint not null, value varchar(255), primary key (parameterId));
+create table dbo.textparametervalue (parameterId bigint not null, value varchar(255), primary key (parameterId));
+alter table dbo.textvalue add column identifier varchar(255);
+alter table dbo.textvalue add column name varchar(255);
+alter table dbo.textvalue add column description varchar(255);
+alter table dbo.unit add column name varchar(255);
+alter table dbo.unit add column link varchar(255);
+create table dbo.xmlfeatparamvalue (parameterId bigint not null, value varchar(max), primary key (parameterId));
+create table dbo.xmlparametervalue (parameterId bigint not null, value varchar(max), primary key (parameterId));
+create index booleanFeatParamIdx on dbo.booleanfeatparamvalue (value);
+create index booleanParamIdx on dbo.booleanparametervalue (value);
+create index categoryFeatParamIdx on dbo.categoryfeatparamvalue (value);
+create index categoryParamIdx on dbo.categoryparametervalue (value);
+create index countFeatParamIdx on dbo.countfeatparamvalue (value);
+create index countParamIdx on dbo.countparametervalue (value);
+create index featureGeomIdx on dbo.featureofinterest USING GIST (geom);
+create index featParamNameIdx on dbo.featureparameter (name);
+create index quantityFeatParamIdx on dbo.numericfeatparamvalue (value);
+create index quantityParamIdx on dbo.numericparametervalue (value);
+create index samplingGeomIdx on dbo.observation USING GIST (samplingGeometry);
+create index paramNameIdx on dbo.parameter (name);
+create index relatedObsObsIdx on dbo.relatedobservation (observationId);
+alter table dbo.series add constraint seriesIdentifierUK unique (identifier);
+create index textFeatParamIdx on dbo.textfeatparamvalue (value);
+create index textParamIdx on dbo.textparametervalue (value);
+alter table dbo.[procedure] add constraint typeOfFk foreign key (typeOf) references dbo.[procedure];
+alter table dbo.booleanfeatparamvalue add constraint featParamBooleanValueFk foreign key (parameterId) references dbo.featureparameter;
+alter table dbo.booleanparametervalue add constraint parameterBooleanValueFk foreign key (parameterId) references dbo.parameter;
+alter table dbo.categoryfeatparamvalue add constraint featParamCategoryValueFk foreign key (parameterId) references dbo.featureparameter;
+alter table dbo.categoryfeatparamvalue add constraint catfeatparamvalueUnitFk foreign key (unitId) references dbo.unit;
+alter table dbo.categoryparametervalue add constraint parameterCategoryValueFk foreign key (parameterId) references dbo.parameter;
+alter table dbo.categoryparametervalue add constraint catParamValueUnitFk foreign key (unitId) references dbo.unit;
+alter table dbo.complexvalue add constraint observationComplexValueFk foreign key (observationId) references dbo.observation;
+alter table dbo.compositeobservation add constraint observationChildFk foreign key (childObservationId) references dbo.observation;
+alter table dbo.compositeobservation add constraint observationParentFK foreign key (observationId) references dbo.complexvalue;
+alter table dbo.countfeatparamvalue add constraint featParamCountValueFk foreign key (parameterId) references dbo.featureparameter;
+alter table dbo.countparametervalue add constraint parameterCountValueFk foreign key (parameterId) references dbo.parameter;
+alter table dbo.featureparameter add constraint FK_4ps6yv41rwnbu3q0let2v7foreign create index (featureOfInterestId) references dbo.featureofinterest;
+alter table dbo.numericfeatparamvalue add constraint featParamNumericValueFk foreign key (parameterId) references dbo.featureparameter;
+alter table dbo.numericfeatparamvalue add constraint quanfeatparamvalueUnitFk foreign key (unitId) references dbo.unit;
+alter table dbo.numericparametervalue add constraint parameterNumericValueFk foreign key (parameterId) references dbo.parameter;
+alter table dbo.numericparametervalue add constraint quanParamValueUnitFk foreign key (unitId) references dbo.unit;
+alter table dbo.offeringrelation add constraint offeringChildFk foreign key (childOfferingId) references dbo.offering;
+alter table dbo.offeringrelation add constraint offeringParenfFk foreign key (parentOfferingId) references dbo.offering;
+alter table dbo.parameter add constraint FK_3v5iovcndi9w0hgh827hcvivw foreign key (observationId) references dbo.observation;
+alter table dbo.profileobservation add constraint profileObsChildFk foreign key (childObservationId) references dbo.observation;
+alter table dbo.profileobservation add constraint profileObsParentFK foreign key (observationId) references dbo.profilevalue;
+alter table dbo.profilevalue add constraint observationProfileValueFk foreign key (observationId) references dbo.observation;
+alter table dbo.profilevalue add constraint profileUnitFk foreign key (levelunitid) references dbo.unit;
+alter table dbo.relatedobservation add constraint FK_g0f0mpuxn3co65uwud4pwxh4q foreign key (observationId) references dbo.observation;
+alter table dbo.relatedobservation add constraint FK_m4nuof4x6w253biuu1r6ttnqc foreign key (relatedObservation) references dbo.observation;
+alter table dbo.series add constraint seriesCodespaceIdentifierFk foreign key (codespace) references dbo.codespace;
+alter table dbo.series add constraint seriesCodespaceNameFk foreign key (codespaceName) references dbo.codespace;
+alter table dbo.textfeatparamvalue add constraint featParamTextValueFk foreign key (parameterId) references dbo.featureparameter;
+alter table dbo.textparametervalue add constraint parameterTextValueFk foreign key (parameterId) references dbo.parameter;
+alter table dbo.xmlfeatparamvalue add constraint featParamXmlValueFk foreign key (parameterId) references dbo.featureparameter;
+alter table dbo.xmlparametervalue add constraint parameterXmlValueFk foreign key (parameterId) references dbo.parameter;
+create sequence dbo.relatedObservationId_seq

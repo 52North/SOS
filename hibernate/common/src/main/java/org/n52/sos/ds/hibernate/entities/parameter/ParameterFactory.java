84979ff4ec5ab9ae970a596af57151c6de21241f
Christian Autermann <c.autermann@52north.org>
2017-06-21 15:25:21 +0200

    fixes for shetland changes

--- hibernate/common/src/main/java/org/n52/sos/ds/hibernate/entities/parameter/ParameterFactory.java
+++ hibernate/common/src/main/java/org/n52/sos/ds/hibernate/entities/parameter/ParameterFactory.java
@@ -45,6 +45,7 @@ import org.n52.shetland.ogc.om.values.SweDataArrayValue;
 import org.n52.shetland.ogc.om.values.TLVTValue;
 import org.n52.shetland.ogc.om.values.TVPValue;
 import org.n52.shetland.ogc.om.values.TextValue;
+import org.n52.shetland.ogc.om.values.TimeRangeValue;
 import org.n52.shetland.ogc.om.values.UnknownValue;
 import org.n52.shetland.ogc.om.values.Value;
 import org.n52.shetland.ogc.om.values.visitor.ValueVisitor;
@@ -193,6 +194,11 @@ public class ParameterFactory implements ValueVisitor<ValuedParameter<?>, OwsExc
 
     @Override
     public ValuedParameter<?> visit(ProfileValue value) throws OwsExceptionReport {
+        throw notSupported(value);
+    }
+
+    @Override
+    public ValuedParameter<?> visit(TimeRangeValue value) throws OwsExceptionReport {
         throw notSupported(value);
     }
 

98fb08e11fc6e83aa97b3a8bddd0c94182e1fc25
Carsten Hollmann <c.hollmann@52north.org>
2019-01-02 10:59:14 +0100

    update for changed mappings for category

--- hibernate/h2/src/main/java/org/n52/sos/ds/hibernate/H2Configuration.java
+++ hibernate/h2/src/main/java/org/n52/sos/ds/hibernate/H2Configuration.java
@@ -133,7 +133,7 @@ public class H2Configuration {
 //            resources.add("/mapping/old/observation/TemporalReferencedObservation.hbm.xml");
 //            resources.add("/mapping/old/observation/ContextualReferencedObservation.hbm.xml");
             resources.add("/mapping/series/observation/ValuedObservation.hbm.xml");
-            resources.add("/mapping/series/observation/Series.hbm.xml");
+            resources.add("/mapping/series/default/Series.hbm.xml");
             resources.add("/mapping/series/observation/RelatedObservations.hbm.xml");
             resources.add("/mapping/series/observation/Observation.hbm.xml");
             resources.add("/mapping/series/observation/TemporalReferencedObservation.hbm.xml");

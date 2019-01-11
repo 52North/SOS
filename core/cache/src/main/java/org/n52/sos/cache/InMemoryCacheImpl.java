59c97abd7a1b193f6d75ad9f6ce2de3ab18361f5
Carsten Hollmann <c.hollmann@52north.org>
2019-01-11 14:51:29 +0100

    Fix NPE in removeFeaturesOfInterestForOffering if no entry there are no values for the offering

--- core/cache/src/main/java/org/n52/sos/cache/InMemoryCacheImpl.java
+++ core/cache/src/main/java/org/n52/sos/cache/InMemoryCacheImpl.java
@@ -1360,8 +1360,10 @@ public class InMemoryCacheImpl extends AbstractStaticContentCache implements Wri
     public void removeFeaturesOfInterestForOffering(final String offering) {
         notNullOrEmpty(OFFERING, offering);
         LOG.trace("Removing featuresOfInterest for offering {}", offering);
-        for (String featureOfInterest : featuresOfInterestForOfferings.get(offering)) {
-            this.offeringsForFeaturesOfInterest.removeWithKey(featureOfInterest, offering);
+        if (featuresOfInterestForOfferings.containsKey(offering)) {
+            for (String featureOfInterest : featuresOfInterestForOfferings.get(offering)) {
+                this.offeringsForFeaturesOfInterest.removeWithKey(featureOfInterest, offering);
+            }
         }
         this.featuresOfInterestForOfferings.remove(offering);
     }

bb5f2682f2eefb9e4b82dc6c62d250b548b0c25e
Carsten Hollmann <c.hollmann@52north.org>
2021-05-26 10:51:52 +0200

    Fix issue with optional resultModel in SOS 1.0.0 GetObservation request

--- hibernate/common/src/main/java/org/n52/sos/ds/hibernate/util/observation/AbstractOmObservationCreator.java
+++ hibernate/common/src/main/java/org/n52/sos/ds/hibernate/util/observation/AbstractOmObservationCreator.java
@@ -181,6 +181,10 @@ public abstract class AbstractOmObservationCreator implements I18nNameDescriptio
         addMetadata(o);
     }
 
+    protected String getResultModel() {
+        return request.getResultModel();
+    }
+
     public abstract ObservationStream create() throws OwsExceptionReport, ConverterException;
 
     private void addMetadata(OmObservation o) {
